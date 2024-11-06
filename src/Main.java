f563247b3058e87e06be0d1dbc2e7f5d4ddcfb92
Alexander-ZAG182 <hado18190@gmail.com>
2024-11-06 22:58:19 +0300

    half commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Задача 1");
+        int age = 13 ;
+        if (age >= 18) {System.out.println("Если возраст человека равен " + age + " он совереннолетний");}
+        else {
+            System.out.println("Если возраст человека равен " + age + " он несовереннолетний"); }
+
+
+        System.out.println("Задача 2");
+        int temperature = 3 ;
+        if (temperature >= 5) {System.out.println("На улице " + temperature + " градусов, можно идти без шапки. ");}
+        else { System.out.println("На улице " + temperature + " градусов,нужно надеть шапку.");}
+
+
+        System.out.println("Задача 3 ");
+        int speed = 61 ;
+        if (speed <= 60) {System.out.println("Если скорость " + speed + " , то можно ездить спокойно");}
+        else { System.out.println("Если скорость " + speed + " придется заплатить штраф.");}
+
+
+        System.out.println("Задача 4 ");
+
+
 
 
 
